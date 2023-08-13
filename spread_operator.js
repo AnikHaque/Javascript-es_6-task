c45271443c40d9d68cd7087f36dc5e2ed5d9bc2f
AnikHaque <ekramul15-811@diu.edu.bd>
2023-08-13 13:04:10 +0600

    updated spread operator

--- spread_operator.js
+++ spread_operator.js
@@ -2,8 +2,8 @@ function mergeArrays(arr1, arr2) {
   return [...arr1, ...arr2];
 }
 
-// Example usage
+
 const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 
-console.log(mergeArrays(arr1, arr2)); // Output: [1, 2, 3, 4, 5, 6]
+console.log(mergeArrays(arr1, arr2)); 
